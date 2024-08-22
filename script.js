ced4f44db8c09a6bd0b7f1864317b90ba668d2ad
CodingWithFiraz <135805970+CodingWithFiraz@users.noreply.github.com>
2024-08-22 13:24:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const navBrand = document.querySelector(".navbar-brand");
+const navBar = document.querySelector(".navbar");
+
+navBrand.addEventListener("click", function () {
+  // body
+  document.body.classList.toggle("dark");
+});
